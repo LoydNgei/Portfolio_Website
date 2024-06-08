aae889d940a7d4549641e2c2b35452908a79629d
LoydNgei <loydngei@gmail.com>
2024-06-08 23:35:32 +0300

    The JS logic

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// // index.js
-// document.addEventListener("DOMContentLoaded", function () {
-//     const navbar = document.querySelector('.navbar');
-//     const menuIcon = document.querySelector('.menu-icon');
-
-//     // Function to toggle the menu
-//     function toggleMenu() {
-//         console.log()
-//         navbar.classList.toggle('show');
-//     }
-
-//     // Add click event listener to the menu icon
-//     menuIcon.addEventListener('click', toggleMenu);
-// });
-
-// script.js
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('show');
